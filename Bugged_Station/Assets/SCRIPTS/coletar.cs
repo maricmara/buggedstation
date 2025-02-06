34fcfe677851824c52a461e5560c2f1b6b888d96
claras7 <145567064+claras7@users.noreply.github.com>
2025-02-06 16:53:06 -0300

    coletavel

---
+++ Bugged_Station/Assets/SCRIPTS/coletar.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coletar : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+    public int score;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            sr.enabled = false;
+            circle.enabled = false;
+            
+            //GameController.instance.totalScore += score;
+            //GameController.instance.UpdateScoreText();
+
+            Destroy(gameObject, 0.25f);
+        }
+    }
+    
+    
+}
