430790ee55d708176c800e0ef1013adb2dcf3866
Samyta Ramiza <145566706+SamytaS2@users.noreply.github.com>
2025-02-19 05:11:23 -0300

    MudançaFase-erro

--- Bugged_Station/Assets/SCRIPTS/NextLevelPoint.cs
+++ Bugged_Station/Assets/SCRIPTS/NextLevelPoint.cs
@@ -8,7 +8,7 @@ public class NextLevelPoint : MonoBehaviour
     public string lvlName;
 
     void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.tag == "PLayer"){
+        if(collision.gameObject.tag == "Player"){
             SceneManager.LoadScene(lvlName);
         }
     }
