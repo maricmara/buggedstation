6e09c552fcd74cfa05081df5cbd977ace955b69b
Cida-Oliveira <145566499+Cida-Oliveira@users.noreply.github.com>
2025-02-20 08:26:25 -0300

    Script da camera

---
+++ Bugged_Station/Assets/SCRIPTS/CameraC.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraC : MonoBehaviour
+{
+    /*public Transform player;
+    public float minX, maxX, minY, maxY, minZ, maxZ;
+    public float lerp;
+
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = player.position + new Vector3(0, 0, 0);
+        newPosition.y = 0.1f;
+        
+        transform.position = Vector3.Lerp(transform.position, newPosition, lerp);
+
+        float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+        float clampedY = Mathf.Clamp(transform.position.y, minY, maxY);
+        float clampedZ = Mathf.Clamp(transform.position.z, minZ, maxZ);
+
+       
+        transform.position = new Vector3(clampedX, clampedY, clampedZ);
+
+
+    }*/
+}
+
+
